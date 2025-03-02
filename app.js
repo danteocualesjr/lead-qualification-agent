c2d21adcd86d113abc6ba2e2982c6d153ba2056e
Your Name <danteocualesjr@gmail.com>
2025-03-02 08:54:34 +0800

    updated vercel.json

--- app.js
+++ app.js
@@ -12,9 +12,21 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Serve the HTML form
+// Serve HTML pages
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/history', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'history.html'));
+});
+
+app.get('/saved', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'saved.html'));
+});
+
+app.get('/settings', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'settings.html'));
 });
 
 // API route to query Perplexity Sonar
